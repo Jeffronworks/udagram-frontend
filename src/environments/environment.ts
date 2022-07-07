78f89a8554ca9dcbcb46482546fb209e19d43d4d
Jeffron <jeffroneu@gmail.com>
2022-07-07 22:43:02 +0100

    added the new apiHost for environments

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://afa772d3865aa46d998c1c0788b536a7-1365458264.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
